0dcefba94c78f2f43ac7cae876900d8cc32b8b68
Maksim Nikolaev <maksim.nikolaev.dev@gmail.com>
2026-04-09 20:14:59 +0200

    change canvas picker from two clicks to click-drag selection

--- ui/canvas_picker.py
+++ ui/canvas_picker.py
@@ -38,30 +38,20 @@ class CanvasPicker:
 
         self.label = self.canvas.create_text(
             screen_w // 2, 60,
-            text="Click the TOP-LEFT corner of your canvas (Esc to cancel)",
+            text="Drag to select your canvas area (Esc to cancel)",
             fill="white", font=("", 14))
 
         self._rect = None
-        self.canvas.bind("<ButtonPress-1>", self._on_click)
-        self.canvas.bind("<Motion>", self._on_motion)
+        self.canvas.bind("<ButtonPress-1>", self._on_press)
+        self.canvas.bind("<B1-Motion>", self._on_drag)
+        self.canvas.bind("<ButtonRelease-1>", self._on_release)
         self.overlay.bind("<Escape>", lambda e: self._cancel())
         self.overlay.focus_force()
 
-    def _on_click(self, event):
-        if self.first_click is None:
-            self.first_click = (event.x_root, event.y_root)
-            self.canvas.itemconfig(self.label,
-                text="Click the BOTTOM-RIGHT corner of your canvas")
-        else:
-            x1, y1 = self.first_click
-            x2, y2 = event.x_root, event.y_root
-            self.overlay.destroy()
-            left, top = min(x1, x2), min(y1, y2)
-            width, height = abs(x2 - x1), abs(y2 - y1)
-            if width > 0 and height > 0:
-                self.on_complete(left, top, width, height)
+    def _on_press(self, event):
+        self.first_click = (event.x_root, event.y_root)
 
-    def _on_motion(self, event):
+    def _on_drag(self, event):
         if self.first_click and self.canvas.winfo_exists():
             if self._rect:
                 self.canvas.delete(self._rect)
@@ -69,6 +59,17 @@ class CanvasPicker:
             self._rect = self.canvas.create_rectangle(
                 x1, y1, event.x_root, event.y_root,
                 outline="lime", width=2, dash=(4, 4))
+
+    def _on_release(self, event):
+        if self.first_click is None:
+            return
+        x1, y1 = self.first_click
+        x2, y2 = event.x_root, event.y_root
+        self.overlay.destroy()
+        left, top = min(x1, x2), min(y1, y2)
+        width, height = abs(x2 - x1), abs(y2 - y1)
+        if width > 0 and height > 0:
+            self.on_complete(left, top, width, height)
 
     def _cancel(self):
         self.overlay.destroy()
